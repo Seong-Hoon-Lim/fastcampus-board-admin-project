e2e9ed09d5b03760dbc277542a0df96df190f98d
Seong-Hoon-Lim <hooney200@gmail.com>
2024-01-21 16:01:12 +0900

    #29 - TestSecurityConfig: 컨트롤러 테스트에 공통으로 들어갈 인증 셋업 추출
    이것으로 모든 controller test 에서 일일이
    `AdminAccountService`를 mocking 하거나,
    인증 세팅을 하는 코드를 표현하지 않아도 됨.
    각 컨트롤러 관심사에서 벗어나는 인증 코드 및 mocking이
    테스트 코드에 드러나지 않게끔 도와줌.

--- board-admin-porject/fastcampus-admin-project/src/test/java/com/fastcampus/adminproject/controller/UserAccountManagementControllerTest.java
+++ board-admin-porject/fastcampus-admin-project/src/test/java/com/fastcampus/adminproject/controller/UserAccountManagementControllerTest.java
@@ -1,6 +1,6 @@
 package com.fastcampus.adminproject.controller;
 
-import com.fastcampus.adminproject.config.SecurityConfig;
+import com.fastcampus.adminproject.config.TestSecurityConfig;
 import com.fastcampus.adminproject.dto.UserAccountDto;
 import com.fastcampus.adminproject.service.UserAccountManagementService;
 import org.junit.jupiter.api.DisplayName;
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("컨트롤러 - 회원 관리")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(UserAccountManagementController.class)
 class UserAccountManagementControllerTest {
 
